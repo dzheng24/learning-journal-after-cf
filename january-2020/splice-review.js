e68c6ea73456ecbd9acaca3ed084c46b21d3d45e
David Zheng <zhengdav@msu.edu>
2020-01-17 17:34:52 -0800

    updated splice review

--- january-2020/splice-review.js
+++ january-2020/splice-review.js
@@ -23,8 +23,33 @@ const seahawks = [
   'shaq'
 ]
 
-// remove 1 element from index 3 
+console.log('original seahawks array: ' + seahawks)
+
+// remove 1 element from index 3, aka remove index 3  
 let removed = seahawks.splice(3, 1);
 console.log(removed);
+console.log('updated seahawks ', seahawks);
+
+// [russ, marshawn, dk, shaq]
+
+// replace marshawn (index 1) to carson
+let removedRB = seahawks.splice(1, 1, 'carson');
+console.log('carson should be in ', seahawks);
+
+// [russ, carson, dk, shaq]
+
+// remove dk from the back, or 1 element from index of -2
+seahawks.splice(-2, 1);
+console.log('no dk', seahawks);
+
+// [russ, carson, shaq]
+
+// remove all elements after index 1(included)
+seahawks.splice(1);
 console.log(seahawks);
+
+// [russ]
+
+
+
 
