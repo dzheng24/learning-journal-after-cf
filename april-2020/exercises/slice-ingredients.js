649e969906ded22d1b87ceccb5b5081b011aabc5
David Zheng <zhengdav@msu.edu>
2020-04-10 14:23:56 -0700

    added grocery shop problem using slice

---
+++ april-2020/exercises/slice-ingredients.js
@@ -0,0 +1,65 @@
+// 4/10/20 Friday 
+
+// Slice Practice 
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 3
+
+You are making a grocery list for ingredients needed in the gruffalo crumble recipe, below. Rather than taking the entire recipe, you only want a list of the item names.
+
+Write a function named listFoods that takes in the recipe and returns a new array of the food items without any amount or units. Just the name. For example, '1 cup flour' will return 'flour'.
+
+Use slice for this function, maybe more than once. The Array.indexOf() method may also be helpful.
+
+Do not use split for this function.
+------------------------------------------------------------------------------------------------ */
+
+const gruffaloCrumble = {
+  name: 'How to make a Gruffalo Crumble',
+  ingredients: [
+    '1 medium-sized Gruffalo',
+    '8 pounds oats',
+    '2 pounds brown sugar',
+    '4 pounds flour',
+    '2 gallons pure maple syrup',
+    '16 cups chopped nuts',
+    '1 pound baking soda',
+    '1 pound baking powder',
+    '1 pound cinnamon',
+    '6 gallons melted butter',
+    '2 gallons fresh water',
+  ],
+  steps: [
+    'Pre-heat a large oven to 375',
+    'De-prickle the gruffalo',
+    'Sprinkle with cinnamon, sugar, flour, and nuts',
+    'Mix until evenly distributed',
+    'Grease a 3-foot x 3-foot casserole dish',
+    'Combine gruffalo compote with water to maintain moisture in the oven',
+    'Fold together remaining ingredients to make the crisp',
+    'Spread the crisp evenly over the gruffalo mixture',
+    'Bake for 12-15 hours',
+  ]
+};
+
+
+const listFoods = (recipe) => {
+  let result = [];
+  // Solution code here...
+  let noNum = []
+  recipe.ingredients.forEach(ingredient => {
+    noNum.push(ingredient.slice(3));
+  })
+  noNum.forEach(ingredient => {
+    for (let i = 0; i < ingredient.length; i++) {
+      if (ingredient[i] === ' ') {
+        result.push(ingredient.slice(i + 1));
+        break;
+      }
+    } 
+  })
+  return result;
+};
+
+
+console.log(listFoods(gruffaloCrumble));
