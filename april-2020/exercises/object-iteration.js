53a30c347fbde028f192181930d5bcfccb3d4461
David Zheng <zhengdav@msu.edu>
2020-04-11 22:03:58 -0700

    added practice problem for object iteration

---
+++ april-2020/exercises/object-iteration.js
@@ -0,0 +1,67 @@
+// 4/11/20 Saturday 
+
+// Object Iteration Practice 
+
+let characters = [
+  {
+    name: 'Eddard',
+    spouse: 'Catelyn',
+    children: ['Robb', 'Sansa', 'Arya', 'Bran', 'Rickon'],
+    house: 'Stark'
+  },
+  {
+    name: 'Jon A.',
+    spouse: 'Lysa',
+    children: ['Robin'],
+    house: 'Arryn'
+  },
+  {
+    name: 'Cersei',
+    spouse: 'Robert',
+    children: ['Joffrey', 'Myrcella', 'Tommen'],
+    house: 'Lannister'
+  },
+  {
+    name: 'Daenarys',
+    spouse: 'Khal Drogo',
+    children: ['Drogon', 'Rhaegal', 'Viserion'],
+    house: 'Targaryen'
+  },
+  {
+    name: 'Mace',
+    spouse: 'Alerie',
+    children: ['Margaery', 'Loras'],
+    house: 'Tyrell'
+  },
+  {
+    name: 'Euron',
+    spouse: null,
+    children: [],
+    house: 'Greyjoy'
+  },
+  {
+    name: 'Jon S.',
+    spouse: null,
+    children: [],
+    house: 'Snow'
+  }
+];
+
+/*------------------------------------------------------------------------------------------------
+CHALLENGE 3
+
+Write a function named hasChildrenValues that uses Object.values to determine if any given character in the data set has children.
+
+This function should take in an array of data and a character name and return a Boolean.
+
+For example:
+hasChildrenValues(characters, 'Cersei') will return true
+hasChildrenValues(characters, 'Sansa') will return false
+------------------------------------------------------------------------------------------------ */
+
+const hasChildrenValues = (arr, character) => {
+  // Solution code here...
+  return arr.some(x => x.name === character && x.children.length)
+};
+
+console.log(hasChildrenValues(characters, 'Daenarys'));
