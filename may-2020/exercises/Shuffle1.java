0dbc9c534a6b3fd23dc14309bb579d7cef3fc7ee
David Zheng <zhengdav@msu.edu>
2020-05-04 18:15:50 -0700

    added journal and exercises from 5/3

---
+++ may-2020/exercises/Shuffle1.java
@@ -0,0 +1,20 @@
+class Shuffle1 {
+  public static void main(String [] args) {
+    int x = 3;
+    while (x > 0) {
+      if (x > 2) {
+        System.out.print("a");
+      }
+      if (x == 2) {
+        System.out.print("b c");
+      }
+      x = x - 1;
+      System.out.print("-");
+      if (x == 1) {
+        System.out.print("d");
+        System.out.println(" ");
+        x = x - 1;
+      }
+    }
+  }
+}
